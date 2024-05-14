3043baf3ebb376ae88a802567714c32188d2b5c5
DebashboraSF <Debash.bora@sourcefuse.com>
2024-05-14 16:37:47 +0530

    changes done after review

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,21 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
-  #   backend "s3" {}
+
 }
 
 provider "aws" {
   region = var.region
 }
-data "aws_ami" "amazon-ami" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
 resource "tls_private_key" "ssh_key_generate" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -36,19 +28,19 @@ resource "aws_key_pair" "generated_key" {
 }
 
 resource "aws_instance" "poc_arc_ec2" {
-  ami                         = data.aws_ami.amazon-ami.id
+  ami                         = var.instance_ami
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   key_name                    = aws_key_pair.generated_key.key_name
   associate_public_ip_address = var.assign_public_ip
-  security_groups = [aws_security_group.instance_sg.id]
-    root_block_device {
+  security_groups             = [aws_security_group.instance_sg.id]
+  root_block_device {
     volume_type           = var.volume_type
     volume_size           = var.volume_size
     encrypted             = var.encrypted
     delete_on_termination = var.delete_on_termination
   }
- tags = {
+  tags = {
     Name = "poc_arc_ec2"
   }
 }
@@ -58,17 +50,22 @@ resource "aws_security_group" "instance_sg" {
 
   vpc_id = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
 }
